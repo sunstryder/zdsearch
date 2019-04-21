2259c117342e969c80b7c1f0dc73f6f74735d995
Tony Sun <tony.sun@beteasy.com.au>
2019-04-21 19:35:39 +1000

    add schemas for the data sets

---
+++ schema/schema.js
@@ -0,0 +1,72 @@
+exports.usersSchema = {
+    "_id": 4,
+    "url": "http://initech.zendesk.com/api/v2/users/4.json",
+    "external_id": "37c9aef5-cf01-4b07-af24-c6c49ac1d1c7",
+    "name": "Rose Newton",
+    "alias": "Mr Cardenas",
+    "created_at": "2016-02-09T07:52:10 -11:00",
+    "active": true,
+    "verified": true,
+    "shared": true,
+    "locale": "de-CH",
+    "timezone": "Netherlands",
+    "last_login_at": "2012-09-25T01:32:46 -10:00",
+    "email": "cardenasnewton@flotonic.com",
+    "phone": "8685-482-450",
+    "signature": "Don't Worry Be Happy!",
+    "organization_id": 122,
+    "tags": [
+      "Gallina",
+      "Glenshaw",
+      "Rowe",
+      "Babb"
+    ],
+    "suspended": true,
+    "role": "end-user"
+}
+
+exports.organizationsSchema = {
+    "_id": 106,
+    "url": "http://initech.zendesk.com/api/v2/organizations/106.json",
+    "external_id": "2355f080-b37c-44f3-977e-53c341fde146",
+    "name": "Qualitern",
+    "domain_names": [
+      "gology.com",
+      "myopium.com",
+      "synkgen.com",
+      "bolax.com"
+    ],
+    "created_at": "2016-07-23T09:48:02 -10:00",
+    "details": "Artisân",
+    "shared_tickets": false,
+    "tags": [
+      "Nolan",
+      "Rivas",
+      "Morse",
+      "Conway"
+    ]
+}
+
+exports.ticketsSchema = {
+    "_id": "87db32c5-76a3-4069-954c-7d59c6c21de0",
+    "url": "http://initech.zendesk.com/api/v2/tickets/87db32c5-76a3-4069-954c-7d59c6c21de0.json",
+    "external_id": "1c61056c-a5ad-478a-9fd6-38889c3cd728",
+    "created_at": "2016-07-06T11:16:50 -10:00",
+    "type": "problem",
+    "subject": "A Problem in Morocco",
+    "description": "Sit culpa non magna anim. Ea velit qui nostrud eiusmod laboris dolor adipisicing quis deserunt elit amet.",
+    "priority": "urgent",
+    "status": "solved",
+    "submitter_id": 14,
+    "assignee_id": 7,
+    "organization_id": 118,
+    "tags": [
+      "Texas",
+      "Nevada",
+      "Oregon",
+      "Arizona"
+    ],
+    "has_incidents": true,
+    "due_at": "2016-08-19T07:40:17 -10:00",
+    "via": "voice"
+  }
