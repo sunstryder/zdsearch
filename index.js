9c6582ec8fc6c544111bdbd8e4ef9f8be7ea18aa
Tony Sun <tony.sun@beteasy.com.au>
2019-04-23 21:33:00 +1000

    add handling for field and value searching!

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
 const vorpal = require('vorpal')();
 const _ = require('lodash');
 const { usersSchema, ticketsSchema, organizationsSchema } = require('./schema/schema.js');
+const users = require('./data/users.json');
+const tickets = require('./data/tickets.json');
+const organizations = require('./data/organizations.json');
 
 // Query for organisation
 vorpal
-	.command('organizations', 'Lists searchable fields in "organization".')
+	.command('organizations [field] [value]', 'Searches for a value of a field in Organizations')
 	.action(function(args, callback){
-		this.log('\nYou can search these fields in "Organizations"\n\n', _.keys(organizationsSchema));
+		let searchField = args.field;
+		let searchValue = args.value;
+
+		if(searchField in organizationsSchema){
+			this.log('Oooh, I found something! Here are your results: ');
+			let searchResults = _.filter(organizations,[searchField,searchValue]);
+			this.log(searchResults);
+		} else {
+			this.log('No organizations like that I\'m afraid');
+		}
 		callback();
 	});
 
 // Query for users
 vorpal
-	.command('users', 'Lists searchable fields in "user".')
-	.action(function(args,callback){
-		this.log('\nYou can search these fields in "Users"\n\n', _.keys(usersSchema));
+	.command('users [field] [value]', 'searches for a value of a field in "user".')
+	.action(function(args,callback){ // Do the search within here.
+		let searchField = args.field;
+		let searchValue = args.value;
+
+		if(searchField in usersSchema){
+			this.log('Oooh, I found something! Here are your results: ');
+			let searchResults = _.filter(users,[searchField,searchValue]);
+			this.log(searchResults);
+		} else {
+			this.log('No users like that I\'m afraid');
+		}
 		callback();
 	});
 
 // Query for tickets
 vorpal
-	.command('tickets', 'Lists searchable fields in "tickets".')
+	.command('tickets [field] [value]', 'searches for a value of a field in "tickets".')
 	.action(function(args,callback){
-		this.log('\nYou can search these fields in "Tickets"\n\n', _.keys(ticketsSchema));
+		let searchField = args.field;
+		let searchValue = args.value;
+
+		if(searchField in ticketsSchema){
+			this.log('Oooh, I found something! Here are your results: ');
+			let searchResults = _.filter(tickets,[searchField,searchValue]);
+			this.log(searchResults);
+		} else {
+			this.log('No tickets like that I\'m afraid');
+		}
 		callback();
 	});
 
-
 // Initial display: show this when app is started
-const displayText = `
+const welcomeText = `
   
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                     ZDSEARCH                    
@@ -44,7 +73,7 @@ You can press tab to autocomplete the command
 enter 'help' at any time to see commands that can be used!
 `;
 
-console.log(displayText);
+console.log(welcomeText);
 
 // start the app
 vorpal
